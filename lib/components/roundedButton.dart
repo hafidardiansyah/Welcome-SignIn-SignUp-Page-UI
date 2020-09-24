e702ae1bf4863b5119680ed0518f90cf6e27df14
unknown <hafidardiansyahh@gmail.com>
2020-09-24 15:12:33 +0700

    addComponents

---
+++ lib/components/roundedButton.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:example/utilities/constants.dart';
+
+class RoundedButton extends StatelessWidget {
+  final String text;
+  final Function press;
+
+  const RoundedButton({
+    Key key,
+    this.press,
+    this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width * 0.8,
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30),
+        child: FlatButton(
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+            onPressed: press,
+            color: lightColor,
+            child: Text(
+              text,
+              style: TextStyle(
+                  color: whiteColor, fontFamily: "Roboto-Bold", fontSize: 17),
+            )),
+      ),
+    );
+  }
+}
---
+++ lib/components/roundedInputField.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:example/utilities/constants.dart';
+
+class RoundedInputField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
