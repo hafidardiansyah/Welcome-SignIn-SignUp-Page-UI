370436a5f1cdbf8b862506d8fa8398148764f27c
unknown <hafidardiansyahh@gmail.com>
2020-09-24 14:07:11 +0700

    createFile

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF1E88E5);
+const lightColor = Color(0xFF6AB7FF);
+const darkColor = Color(0xFF005CB2);
+const whiteColor = Color(0xFFF2F2F2);
+const blackColor = Color(0xFF181A1B);
