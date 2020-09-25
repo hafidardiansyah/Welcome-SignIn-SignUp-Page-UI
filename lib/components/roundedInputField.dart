0f7bbde0a0d20d386977e7609779725d220fc616
unknown <hafidardiansyahh@gmail.com>
2020-09-25 08:41:41 +0700

    create input field

--- lib/components/roundedInputField.dart
+++ lib/components/roundedInputField.dart
@@ -1,9 +1,30 @@
+import 'package:example/components/textFieldContainer.dart';
 import 'package:flutter/material.dart';
 import 'package:example/utilities/constants.dart';
 
 class RoundedInputField extends StatelessWidget {
+  final IconData icon;
+  final String hintText;
+  final ValueChanged<String> onChanged;
+
+  const RoundedInputField({Key key, this.icon, this.hintText, this.onChanged})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return TextFieldContainer(
+      child: TextField(
+        onChanged: onChanged,
+        cursorColor: whiteColor,
+        decoration: InputDecoration(
+            icon: Icon(
+              icon,
+              color: whiteColor,
+            ),
+            hintText: hintText,
+            hintStyle: TextStyle(color: whiteColor, fontFamily: "Poppins"),
+            border: InputBorder.none),
+      ),
+    );
   }
 }
