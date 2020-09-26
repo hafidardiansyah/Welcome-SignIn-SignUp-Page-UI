f37b33fcef2cbc794b637aa55f55e89886ed1e79
unknown <hafidardiansyahh@gmail.com>
2020-09-26 18:21:09 +0700

    add social icon

---
+++ lib/components/socialIcon.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:example/utilities/constants.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+
+class SocialIcon extends StatelessWidget {
+  final String icon;
+  final Function press;
+  const SocialIcon({Key key, this.icon, this.press}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            border: Border.all(
+              width: 2,
+              color: lightColor,
+            ),
+            shape: BoxShape.circle),child: SvgPicture.asset(icon, height: 20, width: 20,),
+      ),
+    );
+  }
+}
